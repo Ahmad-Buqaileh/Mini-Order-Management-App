8d9f6c7a01e06be87bae0f1415101f68e8bb3ad0
Ahmad-Buqaileh <ahmed.bader.buqaileh@gmail.com>
2025-11-17 23:17:40 +0300

    feat(controller): add order controller
    Added new controller:
    
    OrderController: contains method to get user orders, and create order

---
+++ back-end/src/Controller/OrderController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\OrderService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+#[Route('/api/v1/orders')]
+class OrderController extends AbstractController
+{
+    private OrderService $orderService;
+    private ValidatorInterface $validator;
+
+    public function __construct(OrderService $orderService, ValidatorInterface $validator)
+    {
+        $this->orderService = $orderService;
+        $this->validator = $validator;
+    }
+
+    #[Route('/{userId}', name: 'api_orders_get', methods: ['GET'])]
+    public function getUserOrders(string $userId): JsonResponse
+    {
+        try {
+            $orders = $this->orderService->getUserOrders($userId);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'error' => $exception->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return new JsonResponse(
+            $orders,
+            Response::HTTP_OK);
+    }
+
+    #[Route('/{userId}', name: 'api_orders_add', methods: ['POST'])]
+    public function createOrder(string $userId): JsonResponse
+    {
+        try {
+            $order = $this->orderService->createOrderFromUserCart($userId);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'error' => $exception->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+        return $this->json([
+            'success' => true,
+            'order' => [
+                'id' => $order->getId(),
+                'total' => $order->getTotal(),
+                'createdAt' => $order->getCreatedAt(),
+            ],
+        ], Response::HTTP_CREATED);
+    }
+}
