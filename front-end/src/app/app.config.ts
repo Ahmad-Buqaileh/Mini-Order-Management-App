3b0c4867d3b08f539179317e38a3cd98e7541d9a
Ahmad-Buqaileh <ahmed.bader.buqaileh@gmail.com>
2025-11-20 02:27:09 +0300

    chore(app): add global providers and initialize NGRX store with effects

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -3,11 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { ProductsEffects } from './features/result/store/product.effects';
+import { productReducer } from './features/result/store/product.reducers';
+import { cartReducer } from './features/cart/store/cart.reducers';
+import { CartEffects } from './features/cart/store/cart.effects';
+import { orderReducer } from './features/order/store/order.reducers';
+import { OrderEffects } from './features/order/store/order.effects';
+import { SearchEffects } from './features/search/store/search.effects';
+import { searchReducer } from './features/search/store/search.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideStore({products: productReducer, cart: cartReducer,orders: orderReducer, search: searchReducer}),
+    provideEffects([ProductsEffects, CartEffects, OrderEffects, SearchEffects])
+]
 };
