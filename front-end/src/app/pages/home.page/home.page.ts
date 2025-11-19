9f3850241236aa1a65c5ea111d91c70557e1b5a1
Ahmad-Buqaileh <ahmed.bader.buqaileh@gmail.com>
2025-11-20 02:24:49 +0300

    Refactor(home): add other components to home

--- front-end/src/app/pages/home.page/home.page.ts
+++ front-end/src/app/pages/home.page/home.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../components/header.component/header.component'; 
+import { HeaderComponent } from '../../components/header.component/header.component';
+import { SearchComponent } from '../../features/search/components/search.component/search.component';
+import { CategoryComponent } from '../../features/search/components/category.component/category.component';
+import { DisplayComponent } from '../../features/result/components/display.component/display.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SearchComponent, CategoryComponent, DisplayComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
-export class HomePage {
-
-}
+export class HomePage {}
