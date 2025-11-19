1518c199ad6efb13be7cd32bd93c76df7bc0f685
Ahmad-Buqaileh <ahmed.bader.buqaileh@gmail.com>
2025-11-20 02:11:57 +0300

    feat(env): add environment file with API endpoints

---
+++ front-end/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  cartUrl: 'http://localhost:8000/api/v1/carts',
+  cartItemUrl: 'http://localhost:8000/api/v1/cart_items',
+  productUrl: 'http://localhost:8000/api/v1/products',
+  orderUrl: 'http://localhost:8000/api/v1/orders'
+};
