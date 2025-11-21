8b0af27c84f2c79302d07b6d703e4a9b33a38272
Ahmad-Buqaileh <ahmed.bader.buqaileh@gmail.com>
2025-11-21 05:09:27 +0300

    refactor(order): add if user not signed in it will take him to auth page

--- front-end/src/app/features/order/pages/order.history.page/order.history.page.ts
+++ front-end/src/app/features/order/pages/order.history.page/order.history.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DestroyRef, inject } from '@angular/core';
 import { HeaderComponent } from '../../../../components/header.component/header.component';
 import { OrderCardComponent } from '../../components/order.card.component/order.card.component';
 import { Store } from '@ngrx/store';
@@ -7,6 +7,9 @@ import { Observable } from 'rxjs';
 import { OrderState } from '../../store/order.reducers';
 import { AsyncPipe } from '@angular/common';
 import * as OrderActions from '../../store/order.actions';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { selectUserToken } from '../../../auth/store/auth.selectors';
 
 @Component({
   selector: 'app-order.history.page',
@@ -16,8 +19,20 @@ import * as OrderActions from '../../store/order.actions';
 })
 export class OrderHistoryPage implements OnInit {
   orders$!: Observable<OrderState>;
+  userToken$: Observable<string | null>;
   private userId = '019a9609-46d4-7086-9ae9-70c535157d96';
-  constructor(private store: Store) {}
+  private store = inject(Store);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    this.userToken$ = this.store.select(selectUserToken);
+    this.userToken$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((token) => {
+      if (!token) {
+        this.router.navigate(['/auth']);
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.store.dispatch(
