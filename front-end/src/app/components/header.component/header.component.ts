47b397fa406322aa3f9c8c8e98a980e4f30cb0fa
Ahmad-Buqaileh <ahmed.bader.buqaileh@gmail.com>
2025-11-21 05:06:43 +0300

    feat(header): add nav button to cart and order pages

--- front-end/src/app/components/header.component/header.component.ts
+++ front-end/src/app/components/header.component/header.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../../features/auth/store/auth.actions';
+import { selectUserToken } from '../../features/auth/store/auth.selectors';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-component',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, LucideAngularModule, AsyncPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private store = inject(Store);
+  private router = inject(Router);
+  userToken$!: Observable<string | null>;
 
+  constructor() {
+    this.userToken$ = this.store.select(selectUserToken);
+  }
+
+  logout() {
+    this.store.dispatch(AuthActions.logoutUser());
+    setTimeout(() => {
+      this.router.navigate(['/auth']);
+    }, 1000);
+  }
 }
